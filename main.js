a54337fa4075d749280fd9d1b802bb1e2cb09be8
yukazu211 <104954232+yukazu211@users.noreply.github.com>
2022-07-30 17:05:45 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function serch(){
+let sta = document.getElementById('sta');
+// let day = document.getElementById('lb');
+// let text = document.getElementById('output').innerHTML;
+//    let str = "";
+//     const lb = document.form1.lb;
+
+//     for (let i = 0; i < lb.length; i++) {
+//       if (lb[i].checked) {//(lb[i].checked === true)と同じ
+//         str = lb[i].value;
+//         break;
+//       }
+//     }
+//     // document.getElementById("span1").textContent = str;
+
+
+document.getElementById('output').innerHTML = '<img src="https://yukazu211.github.io/table/'+ sta.value   + '.jpg" width="100%">';
+}
